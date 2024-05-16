d623b8ebc10ac3687f36a53e96868d71f32c3dcb
aaronfllr <aaronfllr@gmail.com>
2024-05-16 17:22:12 -0400

    Added confirmation email upon successful purchase

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,68 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { PageHeader } from "../_components/pageHeader";
+import { Table, TableHead, TableHeader, TableRow, TableBody, TableCell } from "@/components/ui/table";
+import db from "@/db/db";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import { MoreVertical } from "lucide-react";
+import { DeleteDropDownItem } from "./_components/UserActions";
+
+function getUsers() {
+    return db.user.findMany({
+        select: {
+            id: true,
+            email: true,
+            orders: { select: { pricePaidInCents: true } }
+        },
+        orderBy: { createdAt: "desc" }, 
+    })
+}
+
+export default function UsersPage() {
+    return (
+        <>
+            <PageHeader>Customers</PageHeader>
+            <UsersTable />
+        </>
+    )
+}
+
+async function UsersTable() {
+    const users = await getUsers()
+
+    if (users.length === 0) return <p>No Customers Found</p>
+
+    return(
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Email</TableHead>
+                    <TableHead>Orders</TableHead>
+                    <TableHead>Value</TableHead>
+                    <TableHead className="w-0">
+                        <span className="sr-only">Actions</span>
+                    </TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {users.map(user => (
+                    <TableRow key={user.id}>
+                        <TableCell>{user.email}</TableCell>
+                        <TableCell>{formatNumber(user.orders.length)}</TableCell>
+                        <TableCell>{formatCurrency(user.orders.reduce((sum, o) => o.pricePaidInCents + sum, 0) / 100) }</TableCell>
+                        <TableCell className="text-center">
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <MoreVertical />
+                                    <span className="sr-only">Actions</span>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent>
+                                    <DeleteDropDownItem id={user.id} />
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    )
+}
--- src/app/webhooks/stripe/route.tsx
+++ src/app/webhooks/stripe/route.tsx
@@ -2,6 +2,7 @@ import db from "@/db/db";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 import { Resend } from "resend";
+import PurchaseReceiptEmail from "@/email/PurchaseReceipt";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 const resend = new Resend(process.env.RESEND_API_KEY as string)
@@ -34,7 +35,8 @@ export async function POST(req: NextRequest) {
             where: { email },
             create: userFields,
             update: userFields,
-            select: { orders: { orderBy: { createdAt: "desc" }, take: 1} },
+            select: { orders: { orderBy: { createdAt: "desc" }, 
+            take: 1} },
         })
 
         const downloadVerification = await db.downloadVerification.create({
@@ -48,7 +50,7 @@ export async function POST(req: NextRequest) {
             from: `Support <${process.env.SENDER_EMAIL}>`,
             to: email,
             subject: "Order Receipt",
-            react: <h1>Thank you for your purchase</h1>,
+            react: <PurchaseReceiptEmail order={order} product={product} downloadVerificationId={downloadVerification.id} />,
         })
     }
 
